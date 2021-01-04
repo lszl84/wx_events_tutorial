6fe1bac3005494f3c96d4ee1f808950c935da1dd
Lukasz <lukasz.sromek@gmail.com>
2021-01-04 13:53:29 +0100

    event skipping

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,22 @@ public:
 
 private:
     void OnClick(wxCommandEvent &);
+    void OnOtherClick(wxCommandEvent &);
 
     wxDECLARE_EVENT_TABLE();
+};
+
+enum ButtonId
+{
+    first_button_id = wxID_LAST + 1,
+    other_button_id
 };
 
 // clang-format off
 
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
 EVT_BUTTON(wxID_ANY, MyFrame::OnClick)
+EVT_BUTTON(other_button_id, MyFrame::OnOtherClick)
 wxEND_EVENT_TABLE()
 
 ; // clang-format on
@@ -36,8 +44,8 @@ bool MyApp::OnInit()
 
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
-    auto button = new wxButton(this, wxID_ANY, "Click me!");
-    auto otherButton = new wxButton(this, wxID_ANY, "Or me!");
+    auto button = new wxButton(this, first_button_id, "Click me!");
+    auto otherButton = new wxButton(this, other_button_id, "Or me!");
 
     auto sizer = new wxBoxSizer(wxHORIZONTAL);
     sizer->Add(button, 0, wxCENTER | wxALL, 100);
@@ -48,4 +56,10 @@ MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size) 
 void MyFrame::OnClick(wxCommandEvent &e)
 {
     std::cout << "Hey, button clicked. It's id = " << e.GetId() << std::endl;
+    e.Skip();
+}
+
+void MyFrame::OnOtherClick(wxCommandEvent &e)
+{
+    std::cout << "Other button clicked. It's id = " << e.GetId() << std::endl;
 }
